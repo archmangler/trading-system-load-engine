26f3f37cc3cadcc436f06eafd669e586c3177add
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-05-09 16:08:00 +0800

    create remote state s3 bucket

--- kubernetes/eks-deploy/deploy.sh
+++ kubernetes/eks-deploy/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 TF_VAR_AWS_CLUSTER_NAME=${AWS_CLUSTER_NAME}
 echo "building eks cluster: $AWS_CLUSTER_NAME"
 
@@ -16,31 +17,27 @@ function install_authenticator () {
 }
 
 function eks_deploy () {
- printf "******************** initializing EKS terraform state ..."
- terraform init
- printf "******************** generating EKS terraform plan ..."
- echo "running: terraform plan -var aws_cluster_name=$TF_VAR_AWS_CLUSTER_NAME -out terraform.plan"
- terraform plan -var aws_cluster_name=$TF_VAR_AWS_CLUSTER_NAME -out terraform.plan
- #TEMPORARY DISABLE
- #terraform apply terraform.plan
+  printf "******************** initializing EKS terraform state ..."
+  terraform init
+  printf "******************** generating EKS terraform plan ..."
+  echo "running: terraform plan -var aws_cluster_name=$TF_VAR_AWS_CLUSTER_NAME -out terraform.plan"
+  terraform plan -var aws_cluster_name=$TF_VAR_AWS_CLUSTER_NAME -out terraform.plan
+  #TEMPORARY DISABLE
+  terraform apply terraform.plan
 }
 
 function get_kube_credentials () {
-
   CMD="aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)"
   printf "Getting kubeconf with: aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)"
   OUT=$($CMD)
   printf "$OUT\n"
-
 }
 
 function health_checks () {
-
   printf "=================== Basic Cluster Checks =================\n"
   kubectl get nodes -o wide
   kubectl get ns
   printf "==========================================================\n"
-
 }
 
 function install_metrics_service () {
