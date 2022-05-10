b2abc73ce851ada193231e00c1d67a9ec45dc215
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-05-10 17:08:20 +0800

    s3 now unified with kubernetes+vpc code

--- storage/eks-deploy/deploy.sh
+++ storage/eks-deploy/deploy.sh
@@ -3,14 +3,14 @@ set -x
 namespace="ragnarok"
 POLICY_ARN="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 
-function s3_deploy () {
- printf "============ begin deploying order data storage archive =================="
- current_dir = `pwd`
- printf "initializing S3 terraform state in $current_dir ..."
- terraform init
- terraform plan -out terraform.plan
- terraform apply terraform.plan
-}
+#function s3_deploy () {
+# printf "============ begin deploying order data storage archive =================="
+# current_dir = `pwd`
+# printf "initializing S3 terraform state in $current_dir ..."
+# terraform init
+# terraform plan -out terraform.plan
+# terraform apply terraform.plan
+#}
 
 function create_eks_storage_access() {
   echo "eksctl utils associate-iam-oidc-provider --cluster=$AWS_CLUSTER_NAME"
@@ -29,5 +29,5 @@ function check_kubernetes_access () {
 }
 
 check_kubernetes_access
-s3_deploy
+#s3_deploy
 create_eks_storage_access
