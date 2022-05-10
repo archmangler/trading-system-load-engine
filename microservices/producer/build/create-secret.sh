e6e0124bba91d45bfe03e2b47e15bd9a6be52c0a
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-05-10 12:01:43 +0800

    create docker hub secret

--- microservices/producer/build/create-secret.sh
+++ microservices/producer/build/create-secret.sh
@@ -3,8 +3,9 @@
 #
 #Set the secret as follows:
 #export DOCKER_PASSWORD=`aws ecr get-login-password --region ap-southeast-1`
-echo "Docker Password => ${DOCKER_PASSWORD}"
+printf "creating docker registry secret for image access ..."
 
+printf "making sure ns is there ..."
 kubectl create namespace ragnarok
 
 #remove old secret ...
@@ -12,9 +13,17 @@ printf "deleting docker image pull secret ..."
 kubectl delete secret ragnarok \
     --namespace ragnarok
 
+#For AWS registry
 #replace old secret
+#kubectl create secret docker-registry ragnarok \
+#    --namespace ragnarok \
+#    --docker-server=605125156525.dkr.ecr.ap-southeast-1.amazonaws.com \
+#    --docker-username=AWS \
+#    --docker-password="${DOCKER_PASSWORD}"
+
+#for dockerhub private registry ...
 kubectl create secret docker-registry ragnarok \
     --namespace ragnarok \
-    --docker-server=605125156525.dkr.ecr.ap-southeast-1.amazonaws.com \
-    --docker-username=AWS \
-    --docker-password="${DOCKER_PASSWORD}"
+    --docker-server=${DOCKERHUB_SERVER} \
+    --docker-username=${DOCKERHUB_USERNAME} \
+    --docker-password="${DOCKERHUB_PASSWORD}"
