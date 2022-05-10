12fee886d5449b829b98b890fbc29f7a67af0497
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-05-10 16:16:54 +0800

    +debugging for redis install

--- microservices/storage/redis-storage/eks-deploy/deploy.sh
+++ microservices/storage/redis-storage/eks-deploy/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #convenient helm install
 set -x
+printf "===================== BEGIN deploying redis cluster from helm chart ..."
 helm install ragnarok charts/redis --namespace redis
+printf "===================== DONE deploying redis cluster from helm chart ..."
