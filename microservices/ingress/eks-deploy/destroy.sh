ec533da3de49aa4acc1f7e2f16dd555c040fc8a2
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-05-31 21:00:39 +0800

    fix ingress deletion bugs

--- microservices/ingress/eks-deploy/destroy.sh
+++ microservices/ingress/eks-deploy/destroy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -x
+INGRESSNAME="ragnarok-ingress"
+namespace="ragnarok"
+#because: https://github.com/kubernetes/kubernetes/issues/95983
+kubectl patch ingress $INGRESSNAME -n $namespace -p '{"metadata":{"finalizers":[]}}' --type=merge
 kubectl delete -f eks-alb-ingress.yaml
