6e0f926f68a8b8baaada59f7c0a0d06004086d70
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-06-16 01:48:48 +0800

    order-dump: update dns fix

--- microservices/order-dumper/build/dnsfix.sh
+++ microservices/order-dumper/build/dnsfix.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#QA
 echo "10.0.9.118 kafka1.dexp-qa.internal" >> /etc/hosts
 echo "10.0.15.240 kafka2.dexp-qa.internal" >> /etc/hosts
 echo "10.0.7.192 kafka3.dexp-qa.internal" >> /etc/hosts
+
+#PT
+echo "10.0.43.66 kafka1.dexp-pt.internal" >> /etc/hosts 
+echo "10.0.46.45 kafka2.dexp-pt.internal" >> /etc/hosts
+echo "10.0.37.33 kafka3.dexp-pt.internal" >> /etc/hosts
